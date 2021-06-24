5ac5e89e84efe4e03fbe9acb52b4881d52b47a50
Allovaro <allovaro@gmail.com>
2021-06-24 16:50:30 +0300

    Crowdsale added

---
+++ contracts/MyTokenSale.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.4;
+
+contract MyCrowdsale is Crowdsale {
+    constructor(
+        uint256 rate,    // rate in TKNbits
+        address payable wallet,
+        IERC20 token
+    )
+        Crowdsale(rate, wallet, token)
+        public
+    {
+
+    }
+}
